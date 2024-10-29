9599ad7e87ee4424ca615ddddee1ca7f3d4c6b44
tomaspinto406 <tomasmartinspinto406@hotmail.com>
2024-10-29 17:42:41 +0000

    commit

---
+++ SiteBiblioteca/Views/Home/Bloquear.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SiteBiblioteca.Views.Home
+{
+    public class BloquearModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
