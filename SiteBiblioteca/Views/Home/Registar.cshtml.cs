5189cb0785f1290d01611e5083f535545c175972
tomaspinto406 <tomasmartinspinto406@hotmail.com>
2024-10-29 14:09:02 +0000

    commit

---
+++ SiteBiblioteca/Views/Home/Registar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SiteBiblioteca.Views.Home
+{
+    public class RegistarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
