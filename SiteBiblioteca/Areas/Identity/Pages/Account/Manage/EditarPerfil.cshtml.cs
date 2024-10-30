983eba6cdb2afdc0cdee4d366de715385b7c09b9
tomaspinto406 <tomasmartinspinto406@hotmail.com>
2024-10-30 14:44:38 +0000

    commit do luis

---
+++ SiteBiblioteca/Areas/Identity/Pages/Account/Manage/EditarPerfil.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SiteBiblioteca.Areas.Identity.Pages.Account.Manage
+{
+    public class EditarPerfilModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
