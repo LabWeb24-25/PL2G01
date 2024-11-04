e7383a868a4fad1cfd6a85e698974bfce6c2d52e
al79171 <al79171@alunos.utad.pt>
2024-11-04 12:13:03 +0000

    Reapply "commit"
    This reverts commit 89285e5c7cd81ea0ea91e9e2a5edb5fd486e3331.

---
+++ SiteBiblioteca/Models/PerfilViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace SiteBiblioteca.Models
+{
+    public class PerfilViewModel
+    {
+        public string Username { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Morada { get; set; }
+        public string Contactos { get; set; }
+    }
+}
