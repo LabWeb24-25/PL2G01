0a9d2d62689f1c239c3a3f74147a314e13badf9f
al79171 <al79171@alunos.utad.pt>
2024-11-04 15:41:26 +0000

    commit

--- SiteBiblioteca/Controllers/HomeController.cs
+++ SiteBiblioteca/Controllers/HomeController.cs
@@ -23,6 +23,12 @@ namespace SiteBiblioteca.Controllers
             return View();
         }
 
+        public IActionResult PersonalData()
+        {
+            return View();
+        }
+
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
