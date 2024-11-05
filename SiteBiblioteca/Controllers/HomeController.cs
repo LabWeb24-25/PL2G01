150db6bfde86b39241f14032f9ea08a21b158b82
tomaspinto406 <tomasmartinspinto406@hotmail.com>
2024-11-05 15:20:30 +0000

    COMMIT

--- SiteBiblioteca/Controllers/HomeController.cs
+++ SiteBiblioteca/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace SiteBiblioteca.Controllers
             return View();
         }
 
+        public IActionResult NotificacoesBibliotecario()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
