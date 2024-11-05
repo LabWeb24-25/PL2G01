bdbbc2c597302f157831855b2fe84e4f17730518
tomaspinto406 <tomasmartinspinto406@hotmail.com>
2024-11-05 12:15:08 +0000

    commit_Recuperar

---
+++ SiteBiblioteca/Views/Home/RecuperarCodigoEmail.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SiteBiblioteca.Views.Home
+{
+    public class RecuperarCodigoEmailModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
