4d3ce8707ded15bd0164118e39f3bc9c38bf9e2d
Rickiard <al79194@alunos.utad.pt>
2024-11-12 09:29:12 +0000

    Tentativa de View com query string;

---
+++ SiteBiblioteca/Controllers/SearchController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace SiteBiblioteca.Controllers
+{
+    public class SearchController : Controller
+    {
+        public IActionResult Pesquisar(string query)
+        {
+            var results = new List<string>();
+
+            // Simulação de lógica de pesquisa
+            if (!string.IsNullOrEmpty(query) && query.Contains("Genshin Impact", StringComparison.OrdinalIgnoreCase))
+            {
+                results.Add("Genshin Impact Art Book");
+            }
+
+            ViewData["SearchTerm"] = query;
+            ViewData["Results"] = results;
+            return View();
+        }
+    }
+}
