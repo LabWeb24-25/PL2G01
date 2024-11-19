4fbb793d5133a4af5a56fe1466006105198fc4f5
Rickiard <al79194@alunos.utad.pt>
2024-11-19 09:26:19 +0000

    Tentativa de view;

--- SiteBiblioteca/Controllers/HomeController.cs
+++ SiteBiblioteca/Controllers/HomeController.cs
@@ -28,5 +28,10 @@ namespace SiteBiblioteca.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult SobreLivro(string ISBN)
+        {
+            return View();
+        }
     }
 }
