ec0da43d04eebf579106c4f2831d969580769084
RICARDO DANIEL DA COSTA TEIXEIRA <al79194@alunos.utad.pt>
2024-11-23 17:53:40 +0000

    Dados associados à base de dados em SobreNos;

--- SiteBiblioteca/Controllers/PagesController.cs
+++ SiteBiblioteca/Controllers/PagesController.cs
@@ -1,15 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SiteBiblioteca.Data;
 using SiteBiblioteca.Models;
 
 namespace SiteBiblioteca.Controllers
 {
     public class PagesController : Controller
     {
-        public IActionResult SobreNos(DadosBiblioteca _data)
-        {
-            _data = new DadosBiblioteca() { contactos = "teste", horario = "teste" };
+        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-            return View(_data);
+        public PagesController(ILogger<HomeController> logger, ApplicationDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public IActionResult SobreNos()
+        {
+            // Busca um registro específico ou o primeiro registro da tabela
+            var dadosBiblioteca = _context._dadosBiblioteca.FirstOrDefault();
+
+            return View(dadosBiblioteca);
         }
 
         public IActionResult RecuperarCodigoEmail()
