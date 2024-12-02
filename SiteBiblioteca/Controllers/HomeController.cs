59bfed8621c1436f15f998012c37025018561147
al79171 <al79171@alunos.utad.pt>
2024-12-02 17:15:17 +0000

    commit

--- SiteBiblioteca/Controllers/HomeController.cs
+++ SiteBiblioteca/Controllers/HomeController.cs
@@ -29,6 +29,9 @@ namespace SiteBiblioteca.Controllers
             return View();
         }
 
+        public IActionResult PainelAdministrador() { return View(); }
+
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
