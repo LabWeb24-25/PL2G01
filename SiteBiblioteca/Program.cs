5188fd1674f21849eefe06622a660c044b5c0d86
Rickiard <al79194@alunos.utad.pt>
2024-12-04 15:31:25 +0000

    ...

--- SiteBiblioteca/Program.cs
+++ SiteBiblioteca/Program.cs
@@ -46,7 +46,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Pages}/{action=PainelAdministrador}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
 
 app.Run();
