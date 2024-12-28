96a43b46732d32584c53a89b66d907cff1ab29dd
RICARDO DANIEL DA COSTA TEIXEIRA <al79194@alunos.utad.pt>
2024-12-28 19:44:17 +0000

    Correção;

--- SiteBiblioteca/Data/ApplicationDbContext.cs
+++ SiteBiblioteca/Data/ApplicationDbContext.cs
@@ -13,8 +13,6 @@ namespace SiteBiblioteca.Data
         public DbSet<User> Adicional { get; set; }
         public DbSet<Requisitar> requisicoes { get; set; }
         public DbSet<Livro> livros { get; set; }
-        public DbSet<CRUD> CRUDs { get; set; }
-        public DbSet<CriarAdministrador> AdministradoresCriados { get; set; }
         public DbSet<Bloqueio> bloqueios { get; set; }
         public DbSet<Autor> autores { get; set; }
         public DbSet<DadosBiblioteca> _dadosBiblioteca { get; set; }
@@ -25,8 +23,6 @@ namespace SiteBiblioteca.Data
 
             modelBuilder.Entity<Bloqueio>()
                 .HasKey(b => new { b.userId, b.adminId, b.dataBloqueio });
-            modelBuilder.Entity<CriarAdministrador>()
-                .HasKey(b => new { b.idCriador, b.idCriado });
             modelBuilder.Entity<Requisitar>()
                 .HasKey(b => new { b.livroISBN, b.leitorId, b.data_requisicao });
         }
